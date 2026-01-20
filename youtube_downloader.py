186dee14489b5c35810339fd1e633d9320676ebb
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-01-20 22:11:23 +0530

    Refactor youtube_downloader into Downloader class
    Refactor youtube downloader to use a Downloader class with improved structure and error handling.

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,123 +1,8 @@
 import yt_dlp
 import os
 import time
-from configs import ffmpeg_location, ffprobe_location, cookies_location
-from file_compressor import compress_audio, compress_mp4
-
-vid_opts = {
-    'format':'bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[vcodec^=avc1]/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'ffmpeg_location': ffmpeg_location,
-    'ffprobe_location':ffprobe_location,
-    'no_mtime': True,
-    'nocache': False,
-    'merge_output_format': 'mp4',
-    "quiet": True,
-    "cachedir": "./cache",
-    'cookiefile':cookies_location,
-    'concurrent_fragment_downloads': 256,
-    "embedsubtitles": True,
-}
-
-subtitles = { # download any subtitles if available preferredly VTT in English.
-    "writesubtitles": True,
-    "subtitleslangs": ["en.*", 'jp.*'],
-    "convertsubtitles": "vtt",
-    'subtitlesformat': "vtt/srt",
-    "embedsubtitles": True,
-    "embedsubtitles": True,
-}
-
-audio_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'ffmpeg_location': ffmpeg_location,
-    'ffprobe_location':ffprobe_location,
-    'no_mtime': True,
-    'nocache': False,
-    "quiet": True,
-    'cookiefile':cookies_location,
-    'concurrent_fragment_downloads': 256,
-    "cachedir": "./cache",
-
-    'postprocessors': [
-        {
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320'
-        },
-        {
-            'key': 'FFmpegMetadata',
-            'add_metadata': {
-                'album': '%(uploader)s', 
-                'genre': 'YouTube - %(upload_date)s', 
-            }
-        },
-    ],
-}
-
-audio_fallback_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'ffmpeg_location': ffmpeg_location,
-    'ffprobe_location':ffprobe_location,
-    'no_mtime': True,
-    'nocache': False,
-    'cookiefile':cookies_location,
-    'concurrent_fragment_downloads': 64,
-    "cachedir": "./cache",
-    "quiet": True,
-    
-    'postprocessors': [
-        {
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192'
-        },
-        {
-            'key': 'FFmpegMetadata',
-            'add_metadata': {
-                'album': '%(uploader)s', 
-                'genre': 'YouTube - %(upload_date)s', 
-            }
-        },
-    ],
-}
-
-video_fallback_opts = {
-    'format': 'bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[vcodec^=avc1]/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'ffmpeg_location': ffmpeg_location,
-    'ffprobe_location':ffprobe_location,
-    'no_mtime': True,
-    'nocache': False,
-    "cachedir": "./cache",
-    "quiet": True,
-    'cookiefile':cookies_location,
-    'concurrent_fragment_downloads': 64,
-    'merge_output_format': 'mp4',
-    "embedsubtitles": True,
-}
-
-search_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'ffmpeg_location': ffmpeg_location,
-    'quiet': True,
-    'noplaylist': True,
-    'nocache': False,
-    'extract_flat': True,
-    "cachedir": "./cache",
-    'concurrent_fragment_downloads': 256,
-}
-
-
-if not os.path.exists("./cache"):
-    os.mkdir("./cache")
-if not os.path.exists("./assets"):
-    os.mkdir("./assets")
-    print("Please add \n'ffmpeg.exe'\nand\n'youtube.com_cookies.txt' files to continue")
-    exit(1)
+import pathlib
+import subprocess
 
 def loader(d):
     if d['status'] == 'downloading':
@@ -125,115 +10,236 @@ def loader(d):
     elif d['status'] == 'finished':
         print("\n[=] Done!")
 
-
-def search(query, total_search=5):
-    print(f"[O] Searching '{query}'...\n")
-    with yt_dlp.YoutubeDL(search_opts) as searcher: # type: ignore
-        results = searcher.extract_info(f"ytsearch{total_search}:{query}", download=False)
-        return results['entries'] # type: ignore
+class Downloader:
+    def __init__(self, ffmpeg="ffmpeg", ffprobe='ffprobe', youtube_cookies_path='youtube_cookies.txt',  soundcloud_cookies_path='soundcloud_cookies.txt', default = 'youtube',
+                 subs_langs = ["en.*", 'jp.*'], JS_runtime_path = './assets/qjs.exe') -> None:
+        self.ffmpeg = ffmpeg
+        self.ffprobe = ffprobe
+        self.yt_cookies = youtube_cookies_path
+        self.sc_cookies = soundcloud_cookies_path
+        self.default = default.lower()
+        self.init(subs_langs=subs_langs, JS_runtime_path=JS_runtime_path)
     
-def download(url, only_audio=True, only_captions=False):
-    if only_audio:
-        options = audio_opts
-    elif only_captions:
-        options = subtitles
-        options['outtmpl'] = '%(title)s.%(ext)s'
-        options['skip_download'] = True
-    else:
-        options = vid_opts
+    def init(self, subs_langs = ["en.*", 'jp.*'], JS_runtime_path = 'qjs.exe'):
+        self.cookies = self.yt_cookies if self.default in ('yt', 'youtube') else self.sc_cookies
+        self.subs_langs = subs_langs
+        self.JS_runtime = JS_runtime_path
 
-    captions = input('Do you want to download the subtitle as .srt (if avaliable)? (Y / N): ').strip().lower() == 'y'
-    options = options.copy()
-    if captions:
-        for k, v in subtitles.items():
-            options[k] = v
-
-    def dynamic_metadata_hook(d):
-        if d['status'] == 'finished':
-            artist_name = d.get('uploader', 'Unknown Artist')
-            d['postprocessor_args'] = [
-                '-metadata', f'artist={artist_name}'
-            ]
-
-    options["progress_hooks"] = [loader, dynamic_metadata_hook]
-    options["js_runtimes"] = {
-        "quickjs": {
-            "path": "./assets/qjs.exe"
-        },
-        "deno": {}
-    }
-    options['remote_components'] = ['ejs:github']
-
-    try:
-        with yt_dlp.YoutubeDL(options) as ydl:  # type: ignore
-            info = ydl.extract_info(url, download=True)
-            file_path = ydl.prepare_filename(info)
-            print(f"\n[√] Saved to: {os.path.abspath(file_path)}")
-            return file_path
-
-    except Exception as e:
-        print(f"[!] Initial download try failed: {e}")
-        os.system("pip install yt-dlp --upgrade")
-        fallback_options = audio_fallback_opts if only_audio else video_fallback_opts
-        fallback_options["js_runtimes"] = {
-        "quickjs": {
-            "path": "./assets/qjs.exe"
-        },
-        "deno": {}
+        self.video_options = {
+        'format':'bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[vcodec^=avc1]/best',
+        'outtmpl': '[SC] %(title)s.%(ext)s' if self.cookies == self.sc_cookies else ('[YT] %(title)s.%(ext)s' if self.cookies == self.yt_cookies else'%(title)s.%(ext)s' ),
+        'ffmpeg_location': self.ffmpeg,
+        'ffprobe_location':self.ffprobe,
+        'no_mtime': True,
+        'nocache': False,
+        'merge_output_format': 'mp4',
+        "quiet": True,
+        "cachedir": "./cache",
+        'cookiefile':self.cookies,
+        'concurrent_fragment_downloads': 256,
+        "embedsubtitles": True,
         }
-        fallback_options['remote_components'] = ['ejs:github']
 
-        if captions:
-            for k, v in subtitles.items():
-                options[k] = v
-        try:
-            with yt_dlp.YoutubeDL(fallback_options) as fallback:  # type: ignore
-                info = fallback.extract_info(url, download=True)
-                file_path = fallback.prepare_filename(info)
-                print(f"\n[√] Saved (fallback) to: {os.path.abspath(file_path)}")
-                return file_path
-        except Exception as e2:
-            print(f"[X] Fallback also failed: {e2}")
-            print("Skipping this item")
-            return None
+        self.subtitles_options = {
+        "writesubtitles": True,
+        "subtitleslangs": self.subs_langs,
+        "convertsubtitles": "vtt",
+        'subtitlesformat': "vtt/srt",
+        "embedsubtitles": True,
+        "embedsubtitles": True,
+        }
 
+        self.audio_options = {
+            'format': 'bestaudio/best',
+            'outtmpl': '[SC] %(title)s.%(ext)s' if self.cookies == self.sc_cookies else ('[YT] %(title)s.%(ext)s' if self.cookies == self.yt_cookies else'%(title)s.%(ext)s' ),
+            'ffmpeg_location': self.ffmpeg,
+            'ffprobe_location':self.ffprobe,
+            'no_mtime': True,
+            'nocache': False,
+            "quiet": True,
+            'cookiefile':self.cookies,
+            'concurrent_fragment_downloads': 256,
+            "cachedir": "./cache",
 
-def main():
-    s = time.time()
-    search_url = input("Do you have the url of the video? (Y/N) ").lower()
-    if search_url == "n":
-        results = search(input("Enter search query: ").strip() or "YouTube")
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '320'
+                },
+                {
+                    'key': 'FFmpegMetadata',
+                    'add_metadata': {
+                        'album': '%(uploader)s', 
+                        'genre': 'YouTube - %(upload_date)s', 
+                    }
+                },
+            ],
+        }
 
-        for idx, vid in enumerate(results):
-            print(f"{idx}. {vid['title']} (- {vid['channel']})")
-
-        print()
-        choice = int(input("Enter choice: "))
-
-        vid_id = results[choice]["id"]
-        url = f"https://www.youtube.com/watch?v={vid_id}"
-
-        print(f"\n[O] Downloading: {results[choice]['title']} (- {results[choice]['channel']})\n")
-    else:
-        url = input("Enter URL: ")
-
-    a = input("Download only Audio (A) or Video with audio (V) or just captions (C)?: ").strip().lower()
-    while a not in ('a', 'v', 'c'):
-        a = input("[!] Invalid choice. Please enter 'A', 'V', or 'C': ").strip().lower()
-
-    file_path = download(url, a=='a', a=='c')
-    if file_path:
-        if input('would you like to compress the file? (Y / N) ').lower().strip() == 'y':
-            if a == 'a':
-                out = compress_audio(file_path)
-            else:
-                out = compress_mp4(file_path)
+        self.audio_fallback_options = {
+            'format': 'bestaudio/best',
+            'outtmpl': '[SC] %(title)s.%(ext)s' if self.cookies == self.sc_cookies else ('[YT] %(title)s.%(ext)s' if self.cookies == self.yt_cookies else'%(title)s.%(ext)s' ),
+            'ffmpeg_location': self.ffmpeg,
+            'ffprobe_location':self.ffprobe,
+            'no_mtime': True,
+            'nocache': False,
+            'cookiefile':self.cookies,
+            'concurrent_fragment_downloads': 64,
+            "cachedir": "./cache",
+            "quiet": True,
             
-            print(f"\n[O] Compressed to {str(out)}")
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192'
+                },
+                {
+                    'key': 'FFmpegMetadata',
+                    'add_metadata': {
+                        'album': '%(uploader)s', 
+                        'genre': 'YouTube - %(upload_date)s', 
+                    }
+                },
+            ],
+        }
+
+        self.video_fallback_options = {
+            'format': 'bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[vcodec^=avc1]/best',
+            'outtmpl': '[SC] %(title)s.%(ext)s' if self.cookies == self.sc_cookies else ('[YT] %(title)s.%(ext)s' if self.cookies == self.yt_cookies else'%(title)s.%(ext)s' ),
+            'ffmpeg_location': self.ffmpeg,
+            'ffprobe_location':self.ffprobe,
+            'no_mtime': True,
+            'nocache': False,
+            "cachedir": "./cache",
+            "quiet": True,
+            'cookiefile':self.cookies,
+            'concurrent_fragment_downloads': 64,
+            'merge_output_format': 'mp4',
+            "embedsubtitles": True,
+        }
+
+        self.search_options = {
+            'format': 'bestaudio/best',
+            'outtmpl': '[SC] %(title)s.%(ext)s' if self.cookies == self.sc_cookies else ('[YT] %(title)s.%(ext)s' if self.cookies == self.yt_cookies else'%(title)s.%(ext)s' ),
+            'ffmpeg_location': self.ffmpeg,
+            'quiet': True,
+            'noplaylist': True,
+            'nocache': False,
+            'extract_flat': True,
+            "cachedir": "./cache",
+            'concurrent_fragment_downloads': 256,
+        }
+
+        if not os.path.exists("./cache"):
+            os.mkdir("./cache")
+        if not os.path.exists("./assets"):
+            os.mkdir("./assets")
+            print(f"Please add \n'ffmpeg.exe'\nand\n'{self.sc_cookies}' or / and '{self.yt_cookies}' files to continue")
+            raise FileNotFoundError(f"Please add \n'ffmpeg.exe'\nand\n'{self.sc_cookies}' or / and '{self.yt_cookies}' files to continue")
+    
+    def search(self, query, total_search=5, platform='youtube'):
+        print(f"[O] Searching '{query}'...\n")
+        with yt_dlp.YoutubeDL(self.search_options) as searcher: # type: ignore
+            search_query = f"ytsearch{total_search}:{query}" if platform.lower() in ('yt', 'youtube') else f"scsearch{total_search}:{query}"
+            results = searcher.extract_info(search_query, download=False)
+            return results['entries'] # type: ignore
+
+    def download(self, url, only_audio=True, only_captions=False, captions = True):
+        if only_audio:
+            options = self.audio_options
+        elif only_captions:
+            options = self.subtitles_options
+            options['outtmpl'] = '%(title)s.%(ext)s'
+            options['skip_download'] = True
         else:
-            print(f"\n[O] File saved to {str(file_path)}")
+            options = self.video_options
 
-    print(f"\n[.] Total time: {time.time() - s:.2f} seconds")
+        options = options.copy()
+        if captions:
+            for k, v in self.subtitles_options.items():
+                options[k] = v
 
-if __name__ == "__main__":
-    main()
+        def dynamic_metadata_hook(d):
+            if d['status'] == 'finished':
+                artist_name = d.get('uploader', 'Unknown Artist')
+                d['postprocessor_args'] = [
+                    '-metadata', f'artist={artist_name}'
+                ]
+
+        options["progress_hooks"] = [loader, dynamic_metadata_hook]
+        options["js_runtimes"] = {
+            "quickjs": {
+                "path": self.JS_runtime
+            },
+            "deno": {}
+        }
+        options['remote_components'] = ['ejs:github']
+
+        try:
+            with yt_dlp.YoutubeDL(options) as ydl:  # type: ignore
+                info = ydl.extract_info(url, download=True)
+                file_path = ydl.prepare_filename(info)
+                print(f"\n[√] Saved to: {os.path.abspath(file_path)}")
+                return file_path
+
+        except Exception as e:
+            print(f"[!] Initial download try failed: {e}")
+            subprocess.run("pip install yt-dlp --upgrade".split())
+            fallback_options = self.audio_fallback_options if only_audio else self.video_fallback_options
+            fallback_options["js_runtimes"] = {
+            "quickjs": {
+                "path": self.JS_runtime
+            },
+            "deno": {}
+            }
+            fallback_options['remote_components'] = ['ejs:github']
+
+            if captions:
+                for k, v in self.subtitles_options.items():
+                    options[k] = v
+            try:
+                with yt_dlp.YoutubeDL(fallback_options) as fallback:  # type: ignore
+                    info = fallback.extract_info(url, download=True)
+                    file_path = fallback.prepare_filename(info)
+                    print(f"\n[√] Saved (fallback) to: {os.path.abspath(file_path)}")
+                    return file_path
+            except Exception as e2:
+                print(f"[X] Fallback also failed: {e2}")
+                print("Skipping this item")
+                return None
+
+    def _compress_audio(self, input_path):
+        input_path = pathlib.Path(input_path)
+        output_path = input_path.with_name(input_path.stem + "_compressed" + input_path.suffix)
+        
+        if input_path.suffix.lower() in [".wav", ".flac"]:
+            subprocess.run([
+                self.ffmpeg,
+                "-i", str(input_path),
+                "-compression_level", "12",
+                str(output_path)
+            ], check=True)
+        else:
+            subprocess.run([
+                self.ffmpeg,
+                "-i", str(input_path),
+                "-b:a", "192k",
+                str(output_path)
+            ], check=True)
+        return output_path
+
+    def _compress_mp4(self, input_path):
+        output_path = pathlib.Path(input_path).with_name(pathlib.Path(input_path).stem + "_compressed.mp4")
+        
+        subprocess.run([
+        self.ffmpeg,
+            "-i", str(input_path),
+            "-c:v", "libx264",
+            "-preset", "slow",
+            "-crf", "18", 
+            "-c:a", "copy",
+            str(output_path)
+        ], check=True)
+        return output_path
