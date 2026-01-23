4adef97de95c9fe07ae2ad4533c0899e5151b766
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-01-23 09:08:42 +0530

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -141,7 +141,7 @@ class Downloader:
             'concurrent_fragment_downloads': 256,
         }
 
-    def update_dlp(self): subprocess.run("pip install yt-dlp --upgrade".split())
+    defupdate_dlp(self): subprocess.run("pip install yt-dlp --upgrade".split())
 
     def init(self, subs_langs = ["en.*", 'jp.*'], JS_runtime_path = 'qjs.exe'):
         self.subs_langs = subs_langs
